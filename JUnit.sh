ff738175a3517270b604b8815ebf7eb626261ba1
Tomáš Beránek <xberan46@stud.fit.vutbr.cz>
2021-03-09 01:25:07 +0100

    add a jar archive instead of class files

--- JUnit.sh
+++ JUnit.sh
@@ -7,4 +7,3 @@ mkdir build
 javac -classpath ${SRC}:junit.jar:ija2020-ukol1.jar -d build ${SRC}/ija/ija2020/homework1/Homework1Test.java
 
 java -cp build:junit.jar:ija2020-ukol1.jar org.junit.runner.JUnitCore ija.ija2020.homework1.Homework1Test
-
